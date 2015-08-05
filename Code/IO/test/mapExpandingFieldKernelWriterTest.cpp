3c36435d67c0ca5b8c9d07290cabd4c363ac6b7f
Ralf Floca <r.floca@dkfz-heidelberg.de>
2015-08-05 15:28:17 +0000

    - fixed Issue #1868

--- Code/IO/test/mapExpandingFieldKernelWriterTest.cpp
+++ Code/IO/test/mapExpandingFieldKernelWriterTest.cpp
@@ -38,135 +38,135 @@
 
 namespace map
 {
-	namespace testing
-	{
+  namespace testing
+  {
 
-		int mapExpandingFieldKernelWriterTest(int argc, char* argv[])
-		{
-			//ARGUMENTS: 1: test storage path
-			//           2: ref path
+    int mapExpandingFieldKernelWriterTest(int argc, char* argv[])
+    {
+      //ARGUMENTS: 1: test storage path
+      //           2: ref path
 
-			PREPARE_DEFAULT_TEST_REPORTING;
+      PREPARE_DEFAULT_TEST_REPORTING;
 
-			std::string testPath = "";
-			std::string refPath = "";
+      std::string testPath = "";
+      std::string refPath = "";
 
-			if (argc > 1)
-			{
-				testPath = argv[1];
-			}
+      if (argc > 1)
+      {
+        testPath = argv[1];
+      }
 
-			if (argc > 2)
-			{
-				refPath = argv[2];
-			}
+      if (argc > 2)
+      {
+        refPath = argv[2];
+      }
 
-			//functor for lazy field kernel
-			typedef TestFieldGenerationFunctor<2, 2> FieldFunctorType;
-			FieldFunctorType::InFieldRepresentationType::SpacingType spacing(0.5);
-			FieldFunctorType::InFieldRepresentationType::PointType origin;
-			origin.Fill(0);
-			FieldFunctorType::InFieldRepresentationType::SizeType size;
-			size.fill(10);
+      //functor for lazy field kernel
+      typedef TestFieldGenerationFunctor<2, 2> FieldFunctorType;
+      FieldFunctorType::InFieldRepresentationType::SpacingType spacing(0.5);
+      FieldFunctorType::InFieldRepresentationType::PointType origin;
+      origin.Fill(0);
+      FieldFunctorType::InFieldRepresentationType::SizeType size;
+      size.fill(10);
 
-			FieldFunctorType::InFieldRepresentationType::Pointer spInRep =
-				FieldFunctorType::InFieldRepresentationType::New();
-			spInRep->setSize(size);
-			spInRep->setSpacing(spacing);
-			spInRep->setOrigin(origin);
-			FieldFunctorType::Pointer spFunctor = FieldFunctorType::New(spInRep);
+      FieldFunctorType::InFieldRepresentationType::Pointer spInRep =
+        FieldFunctorType::InFieldRepresentationType::New();
+      spInRep->setSize(size);
+      spInRep->setSpacing(spacing);
+      spInRep->setOrigin(origin);
+      FieldFunctorType::Pointer spFunctor = FieldFunctorType::New(spInRep);
 
-			//////////////////////////////////////
-			//Kernel setup
-			typedef core::ModelBasedRegistrationKernel<2, 2> KernelType;
+      //////////////////////////////////////
+      //Kernel setup
+      typedef core::ModelBasedRegistrationKernel<2, 2> KernelType;
 
-			typedef core::FieldKernels<2, 2>::LazyFieldBasedRegistrationKernel LazyKernelType;
-			typedef core::FieldKernels<2, 2>::PreCachedFieldBasedRegistrationKernel PreCachedKernelType;
-			typedef core::ModelBasedRegistrationKernel<2, 2> ModelKernelType;
+      typedef core::FieldKernels<2, 2>::LazyFieldBasedRegistrationKernel LazyKernelType;
+      typedef core::FieldKernels<2, 2>::PreCachedFieldBasedRegistrationKernel PreCachedKernelType;
+      typedef core::ModelBasedRegistrationKernel<2, 2> ModelKernelType;
 
-			typedef io::ExpandingFieldKernelWriter<2, 2> WriterType;
-			typedef io::ExpandingFieldKernelWriter<2, 3> Writer23Type;
+      typedef io::ExpandingFieldKernelWriter<2, 2> WriterType;
+      typedef io::ExpandingFieldKernelWriter<2, 3> Writer23Type;
 
-			ModelKernelType::Pointer spModelKernel = ModelKernelType::New();
-			LazyKernelType::Pointer spLazyKernel = LazyKernelType::New();
-			PreCachedKernelType::Pointer spCachedKernel = PreCachedKernelType::New();
+      ModelKernelType::Pointer spModelKernel = ModelKernelType::New();
+      LazyKernelType::Pointer spLazyKernel = LazyKernelType::New();
+      PreCachedKernelType::Pointer spCachedKernel = PreCachedKernelType::New();
 
-			spLazyKernel->setFieldFunctor(*(spFunctor.GetPointer()));
-			LazyKernelType::MappingVectorType nullVector;
-			nullVector[0] = -1;
-			nullVector[1] = -2;
-			spLazyKernel->setNullVector(nullVector);
-			//the cached kernel is not set by purpose now to check the behavior when the field is missing
+      spLazyKernel->setFieldFunctor(*(spFunctor.GetPointer()));
+      LazyKernelType::MappingVectorType nullVector;
+      nullVector[0] = -1;
+      nullVector[1] = -2;
+      spLazyKernel->setNullVector(nullVector);
+      //the cached kernel is not set by purpose now to check the behavior when the field is missing
 
-			spCachedKernel->setNullVectorUsage(false);
+      spCachedKernel->setNullVectorUsage(false);
 
-			////////////////////////////////////////////
-			//Writer setup
-			WriterType::Pointer spWriter = WriterType::New();
+      ////////////////////////////////////////////
+      //Writer setup
+      WriterType::Pointer spWriter = WriterType::New();
 
-			WriterType::RequestType illegalRequest1(spModelKernel, "", "", false);
-			WriterType::RequestType illegalRequest2(spLazyKernel, "", "", false);
-			WriterType::RequestType requestLazy(spLazyKernel, testPath, "ExpandingFieldKernelWriterTest_lazy",
-												true);
-			WriterType::RequestType requestCached(spCachedKernel, testPath,
-												  "ExpandingFieldKernelWriterTest_cached", true);
+      WriterType::RequestType illegalRequest1(spModelKernel, "", "", false);
+      WriterType::RequestType illegalRequest2(spLazyKernel, "", "", false);
+      WriterType::RequestType requestLazy(spLazyKernel, testPath, "ExpandingFieldKernelWriterTest_lazy",
+                                          true);
+      WriterType::RequestType requestCached(spCachedKernel, testPath,
+                                            "ExpandingFieldKernelWriterTest_cached", true);
 
 
-			//////////////////////////////////////
-			//Tests
+      //////////////////////////////////////
+      //Tests
 
-			CHECK_EQUAL(false, spWriter->canHandleRequest(illegalRequest1));
-			CHECK_EQUAL(false, spWriter->canHandleRequest(illegalRequest2));
-			CHECK_EQUAL(true, spWriter->canHandleRequest(requestLazy));
-			CHECK_EQUAL(true, spWriter->canHandleRequest(requestCached));
+      CHECK_EQUAL(false, spWriter->canHandleRequest(illegalRequest1));
+      CHECK_EQUAL(false, spWriter->canHandleRequest(illegalRequest2));
+      CHECK_EQUAL(true, spWriter->canHandleRequest(requestLazy));
+      CHECK_EQUAL(true, spWriter->canHandleRequest(requestCached));
 
-			CHECK_EQUAL("ExpandingFieldKernelWriter<2,2>", spWriter->getProviderName());
-			CHECK_EQUAL("ExpandingFieldKernelWriter<2,3>", Writer23Type::getStaticProviderName());
+      CHECK_EQUAL("ExpandingFieldKernelWriter<2,2>", spWriter->getProviderName());
+      CHECK_EQUAL("ExpandingFieldKernelWriter<2,3>", Writer23Type::getStaticProviderName());
 
-			//test processing of illegal requests
-			CHECK_THROW_EXPLICIT(spWriter->storeKernel(illegalRequest1), core::ServiceException);
-			CHECK_THROW_EXPLICIT(spWriter->storeKernel(illegalRequest2), core::ServiceException);
-			CHECK_THROW(spWriter->storeKernel(requestCached)); //illegal because field is not yet set
+      //test processing of illegal requests
+      CHECK_THROW_EXPLICIT(spWriter->storeKernel(illegalRequest1), core::ServiceException);
+      CHECK_THROW_EXPLICIT(spWriter->storeKernel(illegalRequest2), core::ServiceException);
+      CHECK_THROW(spWriter->storeKernel(requestCached)); //illegal because field is not yet set
 
-			//test valid request
-			structuredData::Element::Pointer spDataLazy;
-			CHECK_NO_THROW(spDataLazy = spWriter->storeKernel(requestLazy));
+      //test valid request
+      structuredData::Element::Pointer spDataLazy;
+      CHECK_NO_THROW(spDataLazy = spWriter->storeKernel(requestLazy));
 
-			//make the cached kernel legal
-			spCachedKernel->setField(*(spLazyKernel->getField()));
-			structuredData::Element::Pointer spDataCached;
-			CHECK_NO_THROW(spDataCached = spWriter->storeKernel(requestCached));
+      //make the cached kernel legal
+      spCachedKernel->setField(*(spLazyKernel->getField()));
+      structuredData::Element::Pointer spDataCached;
+      CHECK_NO_THROW(spDataCached = spWriter->storeKernel(requestCached));
 
-			//test content
-			structuredData::XMLStrWriter::Pointer spStrWriter = structuredData::XMLStrWriter::New();
+      //test content
+      structuredData::XMLStrWriter::Pointer spStrWriter = structuredData::XMLStrWriter::New();
 
-			core::String data = spStrWriter->write(spDataLazy);
-			core::String ref =
-				"<Kernel InputDimensions='2' OutputDimensions='2'><StreamProvider>ExpandingFieldKernelWriter&lt;2,2&gt;</StreamProvider><KernelType>ExpandedFieldKernel</KernelType><FieldPath>ExpandingFieldKernelWriterTest_lazy_field.mhd</FieldPath><UseNullVector>1</UseNullVector><NullVector><Value Row='0'>-1.000000000</Value><Value Row='1'>-2.000000000</Value></NullVector></Kernel>";
+      core::String data = spStrWriter->write(spDataLazy);
+      core::String ref =
+        "<Kernel InputDimensions='2' OutputDimensions='2'><StreamProvider>ExpandingFieldKernelWriter&lt;2,2&gt;</StreamProvider><KernelType>ExpandedFieldKernel</KernelType><FieldPath>ExpandingFieldKernelWriterTest_lazy_field.nrrd</FieldPath><UseNullVector>1</UseNullVector><NullVector><Value Row='0'>-1.000000000</Value><Value Row='1'>-2.000000000</Value></NullVector></Kernel>";
 
-			CHECK_EQUAL(ref, data);
+      CHECK_EQUAL(ref, data);
 
-			//test the fields
-			map::core::String refFieldPath = map::core::FileDispatch::createFullPath(refPath,
-											 "expandingFieldKernelWriterTest_ref.mhd");
-			typedef ::itk::ImageFileReader<LazyKernelType::FieldType> ReaderType;
-			ReaderType::Pointer spReader = ReaderType::New();
-			spReader->SetFileName(refFieldPath);
-			FieldFunctorType::FieldPointer spRefField = spReader->GetOutput();
-			spReader->Update();
+      //test the fields
+      map::core::String refFieldPath = map::core::FileDispatch::createFullPath(refPath,
+                                       "expandingFieldKernelWriterTest_ref.mhd");
+      typedef ::itk::ImageFileReader<LazyKernelType::FieldType> ReaderType;
+      ReaderType::Pointer spReader = ReaderType::New();
+      spReader->SetFileName(refFieldPath);
+      FieldFunctorType::FieldPointer spRefField = spReader->GetOutput();
+      spReader->Update();
 
-			lit::FieldTester<FieldFunctorType::FieldType> tester;
-			double checkThreshold = 0.1;
-			tester.setCheckThreshold(checkThreshold);
-			tester.setExpectedField(spRefField);
+      lit::FieldTester<FieldFunctorType::FieldType> tester;
+      double checkThreshold = 0.1;
+      tester.setCheckThreshold(checkThreshold);
+      tester.setExpectedField(spRefField);
 
-			tester.setActualField(spLazyKernel->getField());
-			CHECK_TESTER(tester);
+      tester.setActualField(spLazyKernel->getField());
+      CHECK_TESTER(tester);
 
-			tester.setActualField(spCachedKernel->getField());
-			CHECK_TESTER(tester);
+      tester.setActualField(spCachedKernel->getField());
+      CHECK_TESTER(tester);
 
-			RETURN_AND_REPORT_TEST_SUCCESS;
-		}
-	} //namespace testing
+      RETURN_AND_REPORT_TEST_SUCCESS;
+    }
+  } //namespace testing
 } //namespace map
