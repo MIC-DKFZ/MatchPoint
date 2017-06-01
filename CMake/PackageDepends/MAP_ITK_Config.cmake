ce785c438d380ee624d9da2e221f6f3148c58d8b
Clemens Hentschke <c.hentschke@dkfz-heidelberg.de>
2017-06-01 15:09:06 +0000

    checked for ITK version (4.6 min)

--- CMake/PackageDepends/MAP_ITK_Config.cmake
+++ CMake/PackageDepends/MAP_ITK_Config.cmake
@@ -8,18 +8,13 @@ IF(DEFINED ITK_DIR)
   ENDIF(NOT IS_ABSOLUTE ${ITK_DIR})
 ENDIF(DEFINED ITK_DIR)
 
-FIND_PACKAGE(ITK)
+FIND_PACKAGE(ITK 4.6 REQUIRED)
 IF(ITK_FOUND)
   INCLUDE(${ITK_USE_FILE})
 ELSE(ITK_FOUND)
   MESSAGE(FATAL_ERROR
           "Cannot build without ITK.  Please set ITK_DIR.")
 ENDIF(ITK_FOUND)
-
-IF(${ITK_VERSION_MAJOR} LESS 4 OR (${ITK_VERSION_MAJOR} EQUAL 4 AND ${ITK_VERSION_MINOR} LESS 0))
-  MESSAGE(FATAL_ERROR
-          "Outdated ITK version. Cannot build MatchPoint without sufficient ITK version. ITK 4.0 or above is needed.")
-ENDIF(${ITK_VERSION_MAJOR} LESS 4 OR (${ITK_VERSION_MAJOR} EQUAL 4 AND ${ITK_VERSION_MINOR} LESS 0))
 
 LIST(APPEND ALL_INCLUDE_DIRECTORIES ${ITK_INCLUDE_DIRS})
 LIST(APPEND ALL_LIBRARIES ${ITK_LIBRARIES})
@@ -29,7 +24,6 @@ LINK_DIRECTORIES(${ITK_LIBRARY_DIRS})
 OPTION(MAP_USE_SYSTEM_GDCM "Activate checker to choose a GDCM installation that should be linked with ITK. (This is needed e.g. when building against an ITK that is distributed with MITK. MITK uses its own GDCM and not the one distributed with ITK.)" OFF)     
 
 IF (MAP_USE_SYSTEM_GDCM)
-
   IF(DEFINED GDCM_DIR)
 	IF(NOT IS_ABSOLUTE ${GDCM_DIR})
 	  SET(GDCM_DIR "${MatchPoint_BINARY_DIR}/${GDCM_DIR}")
